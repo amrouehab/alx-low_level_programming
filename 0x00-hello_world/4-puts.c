694fba86fd2e0d472ba9c3cd8ddefb9daf6d11f5
root <amrou.ehab@gmail.com>
2024-06-20 18:44:41 -0700

    Add all changes including modified and new files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string that includes double quotes
+ *              using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
