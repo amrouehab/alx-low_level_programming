f253da16149cf6d076eeb9137997d356047ff600
root <amrou.ehab@gmail.com>
2024-06-20 18:23:40 -0700

    Add all changes including modified and new files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(void) { printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float)); return (0); }
