146c03aeea248f512457a70d67d148c33d79dfee
root <amrou.ehab@gmail.com>
2024-06-28 20:15:32 -0700

    Add all changes including modified and new files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * fizz_buzz - Prints numbers from 1 to 100 with FizzBuzz conditions
@@ -8,22 +7,63 @@
  */
 void fizz_buzz(void)
 {
-	int i;
+    int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
+        }
+        else if (i % 3 == 0)
+        {
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+        }
+        else if (i % 5 == 0)
+        {
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
+        }
+        else
+        {
+            int num = i;
+            int digits = 0;
 
-		if (i < 100)
-			printf(" ");
-		else
-			printf("\n");
-	}
+            while (num != 0)
+            {
+                num /= 10;
+                digits++;
+            }
+
+            num = i;
+            while (digits > 0)
+            {
+                int divisor = 1;
+
+                for (int j = 1; j < digits; j++)
+                    divisor *= 10;
+
+                _putchar((num / divisor) + '0');
+                num %= divisor;
+                digits--;
+            }
+        }
+
+        if (i < 100)
+            _putchar(' ');
+        else
+            _putchar('\n');
+    }
 }
