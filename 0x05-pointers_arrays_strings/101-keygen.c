584781f9805711b85ef7c88947db8e602f910985
root <amrou.ehab@gmail.com>
2024-07-01 20:50:10 -0700

    Add all changes including modified and new files

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,11 +11,11 @@ int main(void)
 {
     int i;
     int length = 10; // Length of the password
-    char password[length + 1]; // +1 for null terminator
+    char password[11]; // Fixed length array for password, including null terminator
 
     srand(time(0)); // Seed rand() with current time
 
-    // Generate random characters in the ASCII printable range
+    /* Generate random characters in the ASCII printable range */
     for (i = 0; i < length; i++)
     {
         password[i] = rand() % (126 - 33 + 1) + 33;
