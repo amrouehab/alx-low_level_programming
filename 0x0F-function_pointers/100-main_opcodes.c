5952dd4da7e665793a62643575e599b8a255a94d
amrouehab <amrou.ehab@gmail.com>
2024-07-26 13:30:47 -0700

    sol for advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
+#include <stdint.h>
 
 /**
- * print_opcodes - Prints the opcodes of the `main` function.
- * @num_bytes: The number of bytes to print.
+ * main - Prints the opcodes of its own main function.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * 
+ * Return: Always 0 on success, 1 for argument error, and 2 for negative byte count.
  */
-void print_opcodes(int num_bytes)
+int main(int argc, char *argv[])
 {
-    unsigned char *func_ptr;
-    int i;
+    int i, num_bytes;
+    unsigned char *main_ptr;
 
-    func_ptr = (unsigned char *)print_opcodes;
+    /* Check if the number of arguments is correct */
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
 
+    /* Convert the argument to an integer */
+    num_bytes = atoi(argv[1]);
+
+    /* Check if the number of bytes is negative */
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    /* Get the pointer to the start of the main function */
+    main_ptr = (unsigned char *)(uintptr_t)main;
+
+    /* Print the opcodes in hexadecimal format */
     for (i = 0; i < num_bytes; i++)
     {
         if (i > 0)
-        {
             printf(" ");
-        }
-        printf("%02x", func_ptr[i]);
+        printf("%02x", main_ptr[i]);
     }
     printf("\n");
+
+    return (0);
 }
+
