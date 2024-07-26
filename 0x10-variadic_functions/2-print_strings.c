9866fd1e756fa5ec4581eee327961055bcb0ed95
amrouehab <amrou.ehab@gmail.com>
2024-07-26 12:02:39 -0700

    Fix compilation errors

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,27 +3,41 @@
 
 /**
  * print_strings - Prints strings, followed by a new line.
- * @separator: String to be printed between the strings.
+ * @separator: String to be printed between strings.
  * @n: Number of strings passed to the function.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *str;
 	va_list args;
+	char *str;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
+		if (i > 0 && separator != NULL)
+		{
+			const char *sep = separator;
+
+			while (*sep != '\0')
+			{
+				_putchar(*sep);
+				sep++;
+			}
+		}
+
 		str = va_arg(args, char *);
 		if (str == NULL)
 			str = "(nil)";
-		for (; *str != '\0'; str++)
+
+		while (*str != '\0')
+		{
 			_putchar(*str);
-		if (i < n - 1 && separator != NULL)
-			for (; *separator != '\0'; separator++)
-				_putchar(*separator);
+			str++;
+		}
 	}
+
 	_putchar('\n');
 	va_end(args);
 }
